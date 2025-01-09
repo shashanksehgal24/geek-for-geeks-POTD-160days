bf50035c4c9aec0d732b77ac53ad6e90e5f2ed55
Shashank Sehgal <148484893+shashanksehgal24@users.noreply.github.com>
2025-01-09 14:08:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int d = Integer.parseInt(read.readLine().trim());
+
+            Solution ob = new Solution();
+            ArrayList<Integer> result = ob.subarraySum(nums, d);
+            // Print all elements in the result list
+            for (int i : result) {
+                System.out.print(i + " ");
+            }
+            System.out.println(); // Print a new line after the result
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        
+        ArrayList<Integer> list=new ArrayList<>();
+        
+        int s=0,e=0,sum=0;
+        
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+            //sum<target  -> ignore
+            e=i;
+            
+            while(sum>target){
+                sum-=arr[s];
+                ++s;
+            }
+            
+            if(sum==target){
+                list.add(s+1);
+                list.add(e+1);
+                return list;
+            }
+            
+        }
+        list.add(-1);
+        return list;
+        // code here
+    }
+
+}
+
+  
